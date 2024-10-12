f52f08b087b8e349a7ee780a1763e80d37335213
Nikita Chernyaev <oth117@yandex.ru>
2024-10-12 20:38:28 +0300

    Сдача ТЗ проекта № 4

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,40 @@
-public class Main {
+import java.util.ArrayList;
 
+public class Main {
     public static void main(String[] args) {
-        System.out.println("Поехали!");
+        TaskManager taskManager = new TaskManager();
+
+        // Создание задач
+        Task task1 = new Task("Ремонт", "Подготовиться к ремонту", Status.NEW);
+        taskManager.addTask(task1);
+        Task task2 = new Task("Подарок", "Купить подарок", Status.NEW);
+        taskManager.addTask(task2);
+
+        // Создание эпика с подзадачами
+        ArrayList<Integer> subtaskIds = new ArrayList<>();
+        Epic epic1 = new Epic("Задание на работе", "Выполнть важное задание", Status.NEW, subtaskIds);
+        taskManager.addEpic(epic1);
+
+        Subtask subtask1 = new Subtask("Сделать фотографии", "Подготовить фотографии",
+                Status.NEW, epic1.getId());
+        taskManager.addSubtask(subtask1);
+
+        Subtask subtask2 = new Subtask("Создать альбом", "Выбрать подходящие фотографии для альюома",
+                Status.NEW, epic1.getId());
+        taskManager.addSubtask(subtask2);
+
+        System.out.println("Все задачи: " + taskManager.getListOfAllTasks());
+        System.out.println("Все эпики: " + taskManager.getListOfAllEpics());
+        System.out.println("Все подзадачи эпика 1: " + taskManager.getListOfEpicSubstaks(epic1.getId()));
+
+        subtask1.setStatus(Status.DONE);
+        taskManager.updateSubtask(subtask1);
+        System.out.println("Эпик после обновления подзадачи: " + taskManager.getEpicById(epic1.getId()));
+
+        taskManager.removeTaskById(task1.getId());
+        taskManager.removeEpicById(epic1.getId());
+
+        System.out.println("Все задачи после удаления: " + taskManager.getListOfAllTasks());
+        System.out.println("Все эпики после удаления: " + taskManager.getListOfAllEpics());
     }
 }
